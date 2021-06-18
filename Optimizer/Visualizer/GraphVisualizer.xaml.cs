cb29d4ce546959bd516b087f9932bdd49a28d307
Aksel Kvitberg <aksel.kvitberg@outlook.com>
2021-06-18 13:48:47 +0200

    Pan and zoom in window

--- Optimizer/Visualizer/GraphVisualizer.xaml.cs
+++ Optimizer/Visualizer/GraphVisualizer.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using Optimizer.Annotations;
 using Optimizer.Visualizer;
@@ -17,26 +19,29 @@ namespace Optimizer
         {
             InitializeComponent();
             DataContext = this;
-            
+
+            ScaleFactor = 1M;
+
             _assignments = assignments;
             AddNodeInternal(graph.Nodes.First().Value, new Point(100, 100), new Vector(25, 0));
             Score = score;
         }
 
         public List<Node> addedNodes = new List<Node>();
-        
+
         private List<ParkingAssignment> _assignments;
         public Score Score { get; set; }
 
         int rotationindex = 0;
+
         private void AddNodeInternal(Node node, Point point, Vector direction, int rotation = 0)
         {
             var newPoint = Vector.Add(direction, point);
 
-            if(addedNodes.Contains(node))
+            if (addedNodes.Contains(node))
                 return;
 
-            if(node is Source source)
+            if (node is Source source)
             {
                 var nodeThumb = new SourceThumb(source);
                 nodeCanvas.Children.Add(nodeThumb);
@@ -46,7 +51,7 @@ namespace Optimizer
             }
             else
             {
-                var assignment = _assignments.FirstOrDefault(x=>x.ParkingSpot == node);
+                var assignment = _assignments.FirstOrDefault(x => x.ParkingSpot == node);
                 var nodeThumb = new NodeThumb(node, assignment);
                 nodeCanvas.Children.Add(nodeThumb);
                 Canvas.SetLeft(nodeThumb, newPoint.X);
@@ -82,7 +87,7 @@ namespace Optimizer
                     nextRotation = rotation + rotationAngle;
                 }
 
-                
+
                 AddNodeInternal(edge, newPoint, nextDirection, nextRotation);
             }
 
@@ -114,5 +119,99 @@ namespace Optimizer
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+
+        public static readonly DependencyProperty ScaleFactorProperty = DependencyProperty.Register(
+            "ScaleFactor", typeof(Decimal), typeof(GraphVisualizer), new PropertyMetadata(default(Decimal)));
+
+        public Decimal ScaleFactor
+        {
+            get { return (Decimal) GetValue(ScaleFactorProperty); }
+            set { SetValue(ScaleFactorProperty, value); }
+        }
+
+        public static readonly DependencyProperty TranslateXProperty = DependencyProperty.Register(
+            "TranslateX", typeof(int), typeof(GraphVisualizer), new PropertyMetadata(default(int)));
+
+        public int TranslateX
+        {
+            get { return (int) GetValue(TranslateXProperty); }
+            set { SetValue(TranslateXProperty, value); }
+        }
+
+        public static readonly DependencyProperty TranslateYProperty = DependencyProperty.Register(
+            "TranslateY", typeof(int), typeof(GraphVisualizer), new PropertyMetadata(default(int)));
+
+        public int TranslateY
+        {
+            get { return (int) GetValue(TranslateYProperty); }
+            set { SetValue(TranslateYProperty, value); }
+        }
+
+        public static readonly DependencyProperty ScaleOriginXProperty = DependencyProperty.Register(
+            "ScaleOriginX", typeof(int), typeof(GraphVisualizer), new PropertyMetadata(default(int)));
+
+        public int ScaleOriginX
+        {
+            get { return (int) GetValue(ScaleOriginXProperty); }
+            set { SetValue(ScaleOriginXProperty, value); }
+        }
+
+        public static readonly DependencyProperty ScaleOriginYProperty = DependencyProperty.Register(
+            "ScaleOriginY", typeof(int), typeof(GraphVisualizer), new PropertyMetadata(default(int)));
+
+        public int ScaleOriginY
+        {
+            get { return (int) GetValue(ScaleOriginYProperty); }
+            set { SetValue(ScaleOriginYProperty, value); }
+        }
+
+        private Point panningOrigin;
+
+        private void nodeCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            Point position = e.GetPosition(nodeCanvas);
+            if (!IsPanning)
+                return;
+            Vector vector = position - panningOrigin;
+            TranslateX = (int) Math.Round(TranslateX + vector.X);
+            TranslateY = (int) Math.Round(TranslateY + vector.Y);
+        }
+
+        private void nodeCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+            StopPanning();
+        }
+
+        private void nodeCanvas_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+            Point point = PointFromScreen(new Point(window.Left + window.ActualWidth / 2.0, window.Top + window.ActualHeight / 2.0));
+            ScaleOriginX = checked((int) Math.Round(point.X));
+            ScaleOriginY = checked((int) Math.Round(point.Y));
+            ScaleFactor = new Decimal(Math.Min(Math.Max(0.1, Convert.ToDouble(ScaleFactor) + e.Delta / 1000.0), 10.0));
+        }
+
+        private void nodeCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if ((uint) (e.MiddleButton & MouseButtonState.Pressed) <= 0U)
+                return;
+            StartPanning(e.GetPosition(nodeCanvas));
+        }
+
+        private void nodeCanvas_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if ((uint) (~e.MiddleButton & MouseButtonState.Pressed) <= 0U)
+                return;
+            StopPanning();
+        }
+
+        private bool IsPanning => panningOrigin != new Point();
+
+        private void StartPanning(Point origin) => panningOrigin = origin;
+
+        private void StopPanning() => panningOrigin = new Point();
     }
 }
---
+++ Optimizer/Visualizer/VisualizerWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Optimizer.Visualizer
+{
+    public partial class VisualizerWindow : Window
+    {
+        public VisualizerWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void AddGraph(string name, Graph graph, List<ParkingAssignment> assignments, Score score)
+        {
+            TabItem item = new TabItem()
+            {
+                Header = name,
+                Content = new GraphVisualizer(graph, assignments, score)
+            };
+            Tabs.Items.Add(item);
+        }
+    }
+}
